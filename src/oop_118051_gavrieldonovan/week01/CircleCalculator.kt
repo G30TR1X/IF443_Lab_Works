2fff892bd6ec9b26fc786244beea29fe7b717264
G30TR1X <gavriel10donovan@gmail.com>
2026-02-02 15:57:11 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_118051_gavrieldonovan/week01/CircleCalculator.kt
+++ src/oop_118051_gavrieldonovan/week01/CircleCalculator.kt
@@ -1,14 +1,13 @@
 package oop_118051_gavrieldonovan.week01
 
-fun main(args: Array<String>) {
-    // Variable definition
+fun main() {
     val radius: Double = 7.0
     val pi: Double = 3.14
-    // Calculation
+
     var area= pi * radius * radius
+
     // Output Concatenation
     println("Radius: $radius, Area: $area")
-    // Logic check
     println(checkSize(area))
 }
 fun checkSize(area: Double): String = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
