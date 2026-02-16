6aed48b528324f898298d5562fddf8dfc0bdd78d
G30TR1X <gavriel10donovan@gmail.com>
2026-02-16 13:24:46 +0700

    week03: cleanup code

--- src/oop_118051_gavrieldonovan/week03/Main.kt
+++ src/oop_118051_gavrieldonovan/week03/Main.kt
@@ -3,15 +3,11 @@ package oop_118051_gavrieldonovan.week03
 fun main() {
     val e = Employee("Budi")
 
-    // 1. Test validasi salary
-    e.salary = -1000 // Harusnya print error
+    e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
 
-    // 2. Test Encapsulation
     e.increasePerformance()
-    // e.performanceRating = 5 // Coba uncomment, pasti Merah (error)
 
-    // 3. Test Computed Property
     println("Pajak yang harus dibayar: ${e.tax}")
 }
