05e6e8bd1526483db5575a9d5d444c92afade5fc
G30TR1X <gavriel10donovan@gmail.com>
2026-02-23 12:13:49 +0700

    week04: implement super keyword in Car accelerate

--- src/oop_118051_gavrieldonovan/week04/Car.kt
+++ src/oop_118051_gavrieldonovan/week04/Car.kt
@@ -11,4 +11,9 @@ open class Car(
     override fun honk() {
         println("TIN TIN! Mobil $brand lewat!")
     }
+
+    override fun accelerate() {
+        super.accelerate()
+        println("Mobil $brand menggunakan transmisi gigi untuk menambah kecepatan.")
+    }
 }
