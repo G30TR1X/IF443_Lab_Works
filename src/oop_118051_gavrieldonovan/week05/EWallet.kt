d83351d1793f119c52449c3400144d3e7333ccad
G30TR1X <gavriel10donovan@gmail.com>
2026-03-02 13:45:29 +0700

    week05: (task 2) create EWallet class

---
+++ src/oop_118051_gavrieldonovan/week05/EWallet.kt
@@ -0,0 +1,20 @@
+package oop_118051_gavrieldonovan.week05
+
+class EWallet(
+    accountName: String
+) : PaymentMethod(accountName) {
+    var balance: Double = 0.0
+
+    override fun processPayment(amount: Double) {
+        if (balance < amount) {
+            println("Saldo Tidak Cukup")
+            return
+        }
+
+        balance -= amount
+    }
+
+    fun topUp(amount: Double) {
+        balance += amount
+    }
+}
