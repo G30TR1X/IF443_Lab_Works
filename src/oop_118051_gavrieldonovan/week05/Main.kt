f7e10a3b1f513d9f51b32f89c1aa55d5d1289d5d
G30TR1X <gavriel10donovan@gmail.com>
2026-03-02 13:40:23 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/oop_118051_gavrieldonovan/week05/Main.kt
+++ src/oop_118051_gavrieldonovan/week05/Main.kt
@@ -1,6 +1,7 @@
 package oop_118051_gavrieldonovan.week05
 
 fun main() {
+    /*
     // val pegawai1 = Pegawai("Rah")
     val dosen1 = Dosen("Pak Alex", "0123456")
     val admin1 = Admin("Bu Siti")
@@ -23,4 +24,15 @@ fun main() {
         }
     }
     println("------------------------------")
+     */
+
+    println("=== MATH HELPER TEST ===")
+    val mathHelper: MathHelper = MathHelper()
+    val luasPersegi = mathHelper.hitungLuas(4)
+    val luasPersegiPanjang = mathHelper.hitungLuas(5, 6)
+    val luasLingkaran = mathHelper.hitungLuas(7.0)
+
+    println("Luas Persegi dengan sisi 4: $luasPersegi")
+    println("Luas Persegi Panjang dengan panjang 5 dan lebar 6: $luasPersegiPanjang")
+    println("Luas Lingkaran dengan jari jari 7: $luasLingkaran")
 }
