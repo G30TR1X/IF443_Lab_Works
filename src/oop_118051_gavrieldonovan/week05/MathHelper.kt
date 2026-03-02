4586a53e4d1660cdb38ffeaa0c4e6724e61204d1
G30TR1X <gavriel10donovan@gmail.com>
2026-03-02 13:34:35 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_118051_gavrieldonovan/week05/MathHelper.kt
@@ -0,0 +1,9 @@
+package oop_118051_gavrieldonovan.week05
+
+class MathHelper {
+    fun hitungLuas(sisi: Int): Int = sisi * sisi
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int = panjang * lebar
+
+    fun hitungLuas(jarijari: Double): Double = 3.14 * jarijari * jarijari
+}
