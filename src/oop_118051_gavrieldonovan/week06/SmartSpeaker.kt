b16a42e0b40f0bb0e71203ab910db061f0e69a01
G30TR1X <gavriel10donovan@gmail.com>
2026-03-09 11:55:46 +0700

    week06: (task) create SmartSpeaker

---
+++ src/oop_118051_gavrieldonovan/week06/SmartSpeaker.kt
@@ -0,0 +1,18 @@
+package oop_118051_gavrieldonovan.week06
+
+class SmartSpeaker(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable {
+    override fun turnOn() {
+        println("Menyalakan speaker $name, dengan id: $id")
+    }
+
+    override fun turnOff() {
+        println("Mematikan speaker $name, dengan id: $id")
+    }
+
+    fun playMusic(song: String) {
+        println("Mematikan lagu $song dari Spotify.")
+    }
+}
