f06ebcf203802567a5ea83e2a7dd8cba4cabdddd
G30TR1X <gavriel10donovan@gmail.com>
2026-03-09 11:36:31 +0700

    week06: create Smartwatch with multiple implementations

---
+++ src/oop_118051_gavrieldonovan/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_118051_gavrieldonovan.week06
+
+interface Camera {
+    fun turnOn() {
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone {
+    fun turnOn() {
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
