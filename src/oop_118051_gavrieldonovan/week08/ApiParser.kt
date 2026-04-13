9934d91215735c4ab87703790f752ae9b3f358c8
G30TR1X <gavriel10donovan@gmail.com>
2026-04-13 13:34:38 +0700

    week08: (task) implement robust ApiParser using Elvis and requireNotNull

---
+++ src/oop_118051_gavrieldonovan/week08/ApiParser.kt
@@ -0,0 +1,27 @@
+package oop_118051_gavrieldonovan.week08
+
+class ApiParser {
+    fun parseProduct(rawJson: Map<String, Any?>): Product? {
+        try {
+            val id: String = requireNotNull(rawJson["id"]) {
+                "API Invalid: Missing id"
+            } as String
+            val name = requireNotNull(rawJson["name"]) {
+                "API Invalid: Missing name"
+            } as String
+
+            val type = rawJson["type"] as? String
+            if (type == "ELECTRONIC") {
+                val warranty = rawJson["warranty"] as? Int ?: 12
+                return Product.Electronic(id, name, warranty)
+            } else if (type == "CLOTHING") {
+                val size =  rawJson["size"] as? String ?: "All Size"
+                return Product.Clothing(id, name, size)
+            }
+        } catch(e: IllegalArgumentException) {
+            println(e.message)
+        }
+
+        return null
+    }
+}
--- src/oop_118051_gavrieldonovan/week08/EcommerceModels.kt
+++ src/oop_118051_gavrieldonovan/week08/EcommerceModels.kt
@@ -2,5 +2,5 @@ package oop_118051_gavrieldonovan.week08
 
 sealed class Product {
     data class Electronic(val id: String, val name: String, val warrantyMonths: Int) : Product()
-    data class Clothing(val id: String, val name: String, val size: Int) : Product()
+    data class Clothing(val id: String, val name: String, val size: String) : Product()
 }
