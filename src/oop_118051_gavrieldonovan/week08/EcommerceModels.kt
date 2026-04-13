8822a1127b16999e469d0d76300239c4fa9aa720
G30TR1X <gavriel10donovan@gmail.com>
2026-04-13 13:15:50 +0700

    week08: (task) create sealed class Product and data classes

--- src/oop_118051_gavrieldonovan/week08/EcommerceModels.kt
+++ src/oop_118051_gavrieldonovan/week08/EcommerceModels.kt
@@ -1,4 +1,6 @@
 package oop_118051_gavrieldonovan.week08
 
-class EcommerceModels {
+sealed class Product {
+    data class Electronic(val id: String, val name: String, val warrantyMonths: Int) : Product()
+    data class Clothing(val id: String, val name: String, val size: Int) : Product()
 }
