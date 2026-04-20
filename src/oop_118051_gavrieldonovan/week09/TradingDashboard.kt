34993934baa5ec6a8ded955f30f2b7d6b9efe94c
G30TR1X <gavriel10donovan@gmail.com>
2026-04-20 13:41:15 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_118051_gavrieldonovan/week09/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_118051_gavrieldonovan.week09
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "Long", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "Short", 10, -5.2, "CLOSED"),
+        TradeLog("SOLUSDT", "Long", 5, 2.0, "OPEN"),
+        TradeLog("DOTBTC", "Short", 3, 12.8, "CLOSED"),
+        TradeLog("ADAUSDT", "Long", 10, -1.5, "OPEN"),
+        TradeLog("BNBUSDT", "Short", 25, 45.0, "CLOSED")
+    )
+}
