ade1566c04b4f2eb31374421ab830e63b51f5e7a
G30TR1X <gavriel10donovan@gmail.com>
2026-04-27 13:07:12 +0700

    week10: (task) implement search logic in repository

--- src/oop_118051_gavrieldonovan/week10/WalletRepository.kt
+++ src/oop_118051_gavrieldonovan/week10/WalletRepository.kt
@@ -6,4 +6,17 @@ class WalletRepository<T> {
     fun add(item: T) = items.add(item)
 
     fun getAll(): List<T> = items
+
+    fun <T : Any> search(item: T): Boolean {
+        if (item is String) {
+            for (i in items) {
+                if (i.toString() == item.toString())
+                    return true
+            }
+        } else {
+            println("Item has to be a name!")
+        }
+
+        return false;
+    }
 }
