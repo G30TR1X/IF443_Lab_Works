278b6797dabf4f422cf180e69f2b65e8e22d698f
G30TR1X <gavriel10donovan@gmail.com>
2026-04-27 12:53:03 +0700

    week10: implement type constraint for numeric types

---
+++ src/oop_118051_gavrieldonovan/week10/NumericalStorage.kt
@@ -0,0 +1,7 @@
+package oop_118051_gavrieldonovan.week10
+
+class MathBox<T : Number>(val value1: T, val value2: T) {
+    fun sum(): Double {
+        return value1.toDouble() + value2.toDouble()
+    }
+}
