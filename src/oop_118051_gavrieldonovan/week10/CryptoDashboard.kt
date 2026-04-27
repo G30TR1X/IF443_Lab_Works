67b8758f7443b8156904f11906c8b421591e7da9
G30TR1X <gavriel10donovan@gmail.com>
2026-04-27 13:13:01 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_118051_gavrieldonovan/week10/CryptoDashboard.kt
+++ src/oop_118051_gavrieldonovan/week10/CryptoDashboard.kt
@@ -6,4 +6,6 @@ fun main() {
     coinRepo.add(Coin("BTC", 100.0))
     coinRepo.add(Coin("ETH", 100.0))
     coinRepo.add(Coin("USDT", 100.0))
+
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 }
