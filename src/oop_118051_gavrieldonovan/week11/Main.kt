7c253a992eed0b791edeb37f8b37671f6f395bc2
G30TR1X <gavriel10donovan@gmail.com>
2026-05-04 12:29:19 +0700

    week11: test extension functions in main

---
+++ src/oop_118051_gavrieldonovan/week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_118051_gavrieldonovan.week11
+
+fun main() {
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
